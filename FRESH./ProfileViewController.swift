34fa65b2ec26fe7de5febd55de7df40f48b4aa12
Grace Nelson <graceenelson7@gmail.com>
2022-06-30 12:03:12 -0500

    launch page & profile page w name

---
+++ FRESH./ProfileViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileViewController.swift
+//  FRESH.
+//
+//  Created by Scholar on 6/30/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var userName: UITextField!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let newVC = segue.destination as! ViewController
+        newVC.results = userName.text!
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+     
+     
+    }
+    */
+
+}
--- FRESH./ViewController.swift
+++ FRESH./ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var results = "NAME!"
     override func viewDidLoad() {
         super.viewDidLoad()
         let greetings = ["hi,", "hello,", "howdy,", "yo,", "hey,", "welcome,", "hi-ya,", "bonjour,", "hola,", "greetings,", "wassup,", "shalom,", "aloha,"]
@@ -18,8 +18,11 @@ class ViewController: UIViewController {
         greet.text = "\(randomGreeting)"
         
         // Do any additional setup after loading the view.
+        changeName.text = results
+        
     }
     
+    @IBOutlet weak var changeName: UILabel!
     
     var quotes = ["corn is a member of the grass family", "carrots are sweeter in the winter", "raw fruits and vegetables are more nutritious than cooked", "tomatoes are the most popular fruit in the world", "bananas are berries but strawberries aren't", "kiwis contain more vitamin c than oranges", "figs match milk in calcium", "a pomegranate can hold up to 1400 seeds", "the city of chicago is named after garlic. 'chicagaoua' was the indian word for wild garlic", "apples, peaches and raspberries are all members of the rose family", "apples float in water because they are 25% air", "apples give you more energy than coffee", "pineapples are not a single fruit, but a group of berries that are fused to the central stalk", "there is a tree, which bears more than one type of fruit called “the fruit salad tree", "calorie for calorie, there is more protein in broccoli than steak"]
     @IBOutlet weak var quote2: UIButton!
@@ -33,4 +36,3 @@ class ViewController: UIViewController {
     
     
 }
-
