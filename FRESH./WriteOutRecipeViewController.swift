0a9d899972d085cfed370ac353cdf19e77d379a2
Jaritzi Lopez <jnlopez2005@gmail.com>
2022-07-01 11:33:18 -0500

    RECIPE WORKED

--- FRESH./WriteOutRecipeViewController.swift
+++ FRESH./WriteOutRecipeViewController.swift
@@ -8,17 +8,35 @@
 import UIKit
 
 class WriteOutRecipeViewController: UIViewController {
+    
+    var previousVC = RecipesTableViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-  
-    @IBOutlet weak var write_below: UILabel!
-    @IBOutlet weak var write_recipe: UITextField!
     
+        
+        //performSegue(withIdentifier: "go backwards", sender: Any?.self)
+
+    
+    @IBOutlet weak var type_recipe: UITextField!
+   
+   
+    @IBAction func post_recipe(_ sender: Any) {
+        previousVC.recipes.append(type_recipe.text!)
+        print(previousVC.recipes)
+        previousVC.tableView.reloadData()
+        navigationController?.popViewController(animated: true)
+    }
+    //    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//
     /*
     // MARK: - Navigation
 
