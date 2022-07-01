51a35f3848c2ee9a59239b467ee2b84ec9a771b0
Jaritzi Lopez <jnlopez2005@gmail.com>
2022-07-01 12:31:19 -0500

    FINALLLLL

---
+++ AddBlogViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddBlogViewController.swift
+//  FRESH.
+//
+//  Created by Scholar on 7/1/22.
+//
+
+import UIKit
+
+class AddBlogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func type_blog(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
